cb923c3b7bf01de3b39bd3ff4aa20a542d618ead
Alexander Grajales <alexander.grajales@ilimitada.co>
2018-06-19 19:34:23 -0500

    detalle de producto

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from "@shop/products/products.component";
+import { ProductComponent } from '@shop/product/product.component';
 
 
 const routes: Routes = [
-  { path: 'shop', component: ProductsComponent }
+  { path: 'shop', component: ProductsComponent },
+  { path: 'product/:id', component: ProductComponent }
 ];
 
 @NgModule({
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ShopRoutingModule } from './shop-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { SharedModule } from "@shared/shared.module";
+import { SlickModule } from "ngx-slick";
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ShopRoutingModule,
-    SharedModule
+    SharedModule,
+    SlickModule.forRoot(),
   ],
-  declarations: [ProductsComponent]
+  declarations: [ProductsComponent, ProductComponent]
 })
 export class ShopModule { }
